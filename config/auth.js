c642afa333ca781d0482b03126f0a3e839966f6b
akash_10 <42974065+akashmishra96@users.noreply.github.com>
2020-02-29 13:14:19 +0530

    social auth

---
+++ config/auth.js
@@ -0,0 +1,20 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '88144970231198296', //  App ID--r 96
+        'clientSecret'  : '56a678ec57f3f88fec7ec7fb79bbf96b96', //  App Secret --r 96
+        'callbackURL'   : '/auth/facebook/callback'
+        // 'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
+        // 'profileFields' : ['id', 'email', 'name'] // For requesting permissions from Facebook API
+    },
+
+    'googleAuth' : {
+        'clientID'      : '622237229236-sqgu4j67k1g91ml968l23mp459u0n90t96.apps.googleusercontent.com',// --r 96
+        'clientSecret'  : 'wQ1vlEVktjeUFeXfmSLa_Fvz96',// --r 96
+        'callbackURL'   : '/auth/google/callback'
+    }
+
+};
